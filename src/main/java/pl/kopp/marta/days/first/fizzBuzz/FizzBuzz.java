2a2c3956a6da01b06b60ae3fc74e5edf036c3c9b
martazpk <33368716+martazpk@users.noreply.github.com>
2020-03-24 15:56:35 +0100

    refactor

--- src/main/java/pl/kopp/marta/days/first/fizzBuzz/FizzBuzz.java
+++ src/main/java/pl/kopp/marta/days/first/fizzBuzz/FizzBuzz.java
@@ -9,16 +9,16 @@ public class FizzBuzz {
 
     public List<String> game(int range) {
         List<String> result = new ArrayList<>();
-        for (int i = 1; i <=range ; i++) {
+        for (int i = 1; i <= range; i++) {
             String element = "";
 
-            if(isDivisibleByThree(i) || hasNumber(i, '3')){
+            if (isDivisibleByThree(i) || hasNumber(i, "3")) {
                 element = element + THREE_WORD;
             }
-            if(isDivisibleByFive(i)  || hasNumber(i, '5')){
+            if (isDivisibleByFive(i) || hasNumber(i, "5")) {
                 element = element + FIVE_WORD;
             }
-            if(element.isEmpty()) {
+            if (element.isEmpty()) {
                 element = String.valueOf(i);
             }
             result.add(element);
@@ -26,17 +26,9 @@ public class FizzBuzz {
         return result;
     }
 
-    private boolean hasNumber(int i, char number) {
-        if(i > 9){
-            String numberAsString =  String.valueOf(i);
-            char[] chars = numberAsString.toCharArray();
-            for (char c: chars) {
-               if(c == number) {
-                   return true;
-               }
-            }
-        }
-        return false;
+    private boolean hasNumber(int i, String number) {
+            String numberAsString = String.valueOf(i);
+            return numberAsString.contains(number);
     }
 
     private boolean isDivisibleByFive(int i) {
