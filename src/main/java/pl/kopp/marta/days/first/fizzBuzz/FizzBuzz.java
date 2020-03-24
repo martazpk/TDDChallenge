de3b7d78304d84ec120d725ed3a5a13a3209d563
martazpk <33368716+martazpk@users.noreply.github.com>
2020-03-24 08:53:00 +0100

    extend key words

--- src/main/java/pl/kopp/marta/days/first/fizzBuzz/FizzBuzz.java
+++ src/main/java/pl/kopp/marta/days/first/fizzBuzz/FizzBuzz.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FizzBuzz {
+    private static final String THREE_WORD = "Fizz";
+    private static final String FIVE_WORD = "Buzz";
 
     public List<String> game(int range) {
         List<String> result = new ArrayList<>();
@@ -11,10 +13,10 @@ public class FizzBuzz {
             String element = "";
 
             if(isDivisibleByThree(i) || hasNumber(i, '3')){
-                element = element + "Fizz";
+                element = element + THREE_WORD;
             }
             if(isDivisibleByFive(i)  || hasNumber(i, '5')){
-                element = element + "Buzz";
+                element = element + FIVE_WORD;
             }
             if(element.isEmpty()) {
                 element = String.valueOf(i);
