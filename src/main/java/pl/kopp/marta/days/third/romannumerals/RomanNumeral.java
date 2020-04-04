0f7e82f83b1b9dec48dce75c15e77eb8265ea9b4
martazpk <33368716+martazpk@users.noreply.github.com>
2020-04-04 17:59:42 +0200

    cases: 400 -> CD 500 -> D 900 -> CM 1000 -> M

--- src/main/java/pl/kopp/marta/days/third/romannumerals/RomanNumeral.java
+++ src/main/java/pl/kopp/marta/days/third/romannumerals/RomanNumeral.java
@@ -10,9 +10,13 @@ public enum RomanNumeral {
     IX(9),
     X(10),
     XL(40),
+    L(50),
     XC(90),
     C(100),
-    L(50);
+    CD(400),
+    D(500),
+    CM(900),
+    M(1000);
 
     private int value;
 
--- src/test/java/pl/kopp/marta/days/third/romannumerals/RomanNumeralsTest.java
+++ src/test/java/pl/kopp/marta/days/third/romannumerals/RomanNumeralsTest.java
@@ -33,7 +33,11 @@ class RomanNumeralsTest {
             "40,XL",
             "50,L",
             "90,XC",
-            "100,C"
+            "100,C",
+            "400,CD",
+            "500,D",
+            "900,CM",
+            "1000,M"
     })
     void shouldReturnCorrectRomanNumeralIWhenNumberIsGiven(int input, String expected) {
         String romanNumeral = romanNumerals.toRoman(input);
