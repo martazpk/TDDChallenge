ee38996d1967cbd05b2c2336dd98bb34597f3d84
martazpk <33368716+martazpk@users.noreply.github.com>
2020-04-04 15:54:56 +0200

    refactor extract constants

--- src/main/java/pl/kopp/marta/days/third/romannumerals/RomanNumerals.java
+++ src/main/java/pl/kopp/marta/days/third/romannumerals/RomanNumerals.java
@@ -2,20 +2,27 @@ package pl.kopp.marta.days.third.romannumerals;
 
 
 class RomanNumerals {
+    private static final String ONE = "I";
+    private static final String FIVE = "V";
+    private static final String FOUR = "IV";
 
     String toRoman(int i) {
-        if(i == 1) {
-            return "I";
-        }
-        if(i == 2) {
-            return "II";
-        }
-        if(i == 3){
-            return "III";
-        }
-        if(i == 5){
-            return "V";
-        }
-        else return "IV";
+        String result = "";
+
+        if (i <= 3) {
+            result = addOne(i, result);
+        } else if (i == 4) {
+            result = result.concat(FOUR);
+
+        } else result = result.concat(FIVE);
+        return result;
     }
+
+    private String addOne(int i, String result) {
+        for (int j = 1; j <= i; j++) {
+            result = result.concat(ONE);
+        }
+        return result;
+    }
+
 }
