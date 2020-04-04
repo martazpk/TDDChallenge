57659b08aee0488bbab826666b9def32de91783c
martazpk <33368716+martazpk@users.noreply.github.com>
2020-04-04 17:01:23 +0200

    refactor- tests: all in one

--- src/test/java/pl/kopp/marta/days/third/romannumerals/RomanNumeralsTest.java
+++ src/test/java/pl/kopp/marta/days/third/romannumerals/RomanNumeralsTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RomanNumeralsTest {
-private RomanNumerals romanNumerals;
+    private RomanNumerals romanNumerals;
 
     @BeforeEach
     void setUp() {
@@ -16,51 +16,22 @@ private RomanNumerals romanNumerals;
     }
 
     @ParameterizedTest
-    @CsvSource({"1,I", "2,II", "3,III"})
-    void shouldReturnMultipleOfIWhenNumberIsGiven(int input, String expected) {
-        String romanNumeral = romanNumerals.toRoman(input);
-
-        assertThat(romanNumeral).isEqualTo(expected);
-    }
-    @Test
-    void shouldReturnIVWhen4IsGiven() {
-        String romanNumeral = romanNumerals.toRoman(4);
-
-        assertThat(romanNumeral).isEqualTo("IV");
-    }
-
-    @Test
-    void shouldReturnVWhen5IsGiven() {
-        String romanNumeral = romanNumerals.toRoman(5);
-
-        assertThat(romanNumeral).isEqualTo("V");
-    }
-
-    @ParameterizedTest
-    @CsvSource({"6,VI", "7,VII", "8,VIII"})
-    void shouldReturnVIWhen6IsGiven(int input, String expected) {
-        String romanNumeral = romanNumerals.toRoman(input);
-
-        assertThat(romanNumeral).isEqualTo(expected);
-    }
-
-    @Test
-    void shouldReturnIXWhen9IsGiven() {
-        String romanNumeral = romanNumerals.toRoman(9);
-
-        assertThat(romanNumeral).isEqualTo("IX");
-    }
-
-    @Test
-    void shouldReturnXWhen10IsGiven() {
-        String romanNumeral = romanNumerals.toRoman(10);
-
-        assertThat(romanNumeral).isEqualTo("X");
-    }
-
-    @ParameterizedTest
-    @CsvSource({"11,XI", "12,XII", "13,XIII"})
-    void shouldReturnCorrectLiteralWhen11To13IsGiven(int input, String expected) {
+    @CsvSource({
+            "1,I",
+            "2,II",
+            "3,III",
+            "4,IV",
+            "5,V",
+            "6,VI",
+            "7,VII",
+            "8,VIII",
+            "9,IX",
+            "10,X",
+            "11,XI",
+            "12,XII",
+            "13,XIII"
+    })
+    void shouldReturnCorrectRomanNumeralIWhenNumberIsGiven(int input, String expected) {
         String romanNumeral = romanNumerals.toRoman(input);
 
         assertThat(romanNumeral).isEqualTo(expected);
